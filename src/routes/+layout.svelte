bd4098cac89c4d574f15dd511776312fde58a9a1
tithe <tith3@proton.me>
2024-03-12 02:30:47 -0400

    Added lightswitch and header controls. Some other small changes.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,16 +6,21 @@
 	import { AppBar, storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
-	import { AppShell } from '@skeletonlabs/skeleton';
+	import { AppShell, LightSwitch} from '@skeletonlabs/skeleton';
 	import Icon from '$lib/components/Icon.svelte';
+	import HeaderControls from '$lib/components/HeaderControls.svelte';
 </script>
 
 <AppShell>
 	<svelte:fragment slot="header">
-		<AppBar padding='p-3'>
+		<AppBar padding='py-0 px-4 '>
 			<svelte:fragment slot="lead">
-				<Icon width='12' height='12'/>
+				<Icon width='10' height='10'/>
 				<p class='text-xl uppercase p-4'> liquid </p>
+			</svelte:fragment>
+			<HeaderControls />
+			<svelte:fragment slot="trail">
+				<LightSwitch/>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
