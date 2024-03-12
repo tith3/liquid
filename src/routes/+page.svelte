aea2fafa9f185e0a1988ef4850fa19bd95c1bdfa
tithe <tith3@proton.me>
2024-03-11 23:34:28 -0400

    Basic panes with formatting for testing.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
 <script lang=ts>
-	
+	import {Pane, Splitpanes} from "svelte-splitpanes"
+	import '$lib/css/splitpaneTheme.postcss'
 </script>
+
+<Splitpanes theme="skeleton" class="p-4" horizontal={true}>
+	<Pane minSize={10}> One </Pane>
+	<Pane minSize={10}> Two </Pane>
+</Splitpanes>
+
+
