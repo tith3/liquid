2355035ced65ed9d10891e8c0d386ab10c0898b4
tithe <tith3@proton.me>
2024-03-12 16:25:15 -0400

    Added paneDimensions and some other functions to keep track of pane layout and such.

--- src/lib/stores/panes.ts
+++ src/lib/stores/panes.ts
@@ -1,11 +1,24 @@
 import {localStorageStore} from '@skeletonlabs/skeleton'
 import { type Writable, get } from 'svelte/store'
-import type { PaneData} from '../types'
+import type { PaneData, PaneDimensions } from '../types'
+import { Pane } from 'svelte-splitpanes';
 
 export const panes: Writable<PaneData[]> = localStorageStore('panes', [] as PaneData[])
-export const maxPanes = 9;
+
+
+//default pane dimensions
+const defaultPaneDimensions: PaneDimensions = {
+    rows: 3,
+    cols: 3,
+    maxRows: 3,
+    maxCols: 3,
+    vertical: false,
+}
+//store the dimensions of the panes
+export const paneDimensions: Writable<PaneDimensions> = localStorageStore('paneDimensions', defaultPaneDimensions)
 
 export function addPane(text: string) {
+    const maxPanes = get(paneDimensions).rows * get(paneDimensions).cols
     if (get(panes).length >= maxPanes) {
         return;
     }
@@ -38,3 +51,11 @@ export function resetPanes() {
     panes.set([newPane])
 }
 
+export function shrinkPanes() {
+    const maxPanes = get(paneDimensions).rows * get(paneDimensions).cols
+
+    //remove panes with highest indices until number of panes is less than or equal to maxPanes
+    panes.update((panes) => {
+        return panes.slice(0, maxPanes)
+    })
+}
