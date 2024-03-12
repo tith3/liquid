dfef67f15f2f3ee19460feb8e1000c02d2e6b89a
tithe <tith3@proton.me>
2024-03-12 16:27:37 -0400

    Reworked pane construction to function with new pane settings. Functional.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import {Pane, Splitpanes} from "svelte-splitpanes"
 	import '$lib/css/splitpaneTheme.postcss'
 	import PaneContents from '$lib/components/PaneContents.svelte'
-	import {panes, addPane} from '$lib/stores/panes'
+	import {panes, addPane, paneDimensions} from '$lib/stores/panes'
 	import {onMount} from 'svelte'
 	import { derived, get } from 'svelte/store'
 	let rowArr = [0]
@@ -19,30 +19,56 @@
 		}
 		)
 	}
+		
+	$: paneCols = derived(panes, $panes => {
+		let cols = [];
+		let colCount = Math.ceil($panes.length / $paneDimensions.rows);
+		for (let i = 0; i < colCount; i++) {
+			cols.push($panes.slice(i * $paneDimensions.rows, (i + 1) * $paneDimensions.rows));
+		}
+		return cols;
+	});
 
 	$: paneRows = derived(panes, $panes => {
-        let rows = [];
-        let rowCount = Math.ceil($panes.length / 3);
-        for (let i = 0; i < rowCount; i++) {
-            rows.push($panes.slice(i * 3, (i + 1) * 3));
-        }
-        return rows;
-    });
-		
+		let rows = [];
+		let rowCount = Math.ceil($panes.length / $paneDimensions.cols);
+		for (let i = 0; i < rowCount; i++) {
+			rows.push($panes.slice(i * $paneDimensions.cols, (i + 1) * $paneDimensions.cols));
+		}
+		return rows;
+	});
+	
 </script>
 
-<Splitpanes theme="skeleton" class="p-4" horizontal={true}>
-	{#each $paneRows as rowPanes, i}
-        <Pane minSize={2}>
-            <Splitpanes theme="skeleton" horizontal={false}>
-                {#each rowPanes as pane, j}
-                    <Pane minSize={2}>
-                        <PaneContents text={pane.text} index={pane.index}/>
-                    </Pane>
-                {/each}
-            </Splitpanes>
-        </Pane>
-    {/each}
-</Splitpanes>
+{#if $paneDimensions.vertical}
+	<Splitpanes theme="" class="p-4" horizontal={false}>
+		{#each $paneCols as colPanes, i}
+			<Pane minSize={2}>
+				<Splitpanes theme="" horizontal={true}>
+					{#each colPanes as pane, j}
+						<Pane minSize={2}>
+							<PaneContents text={pane.text} index={pane.index}/>
+						</Pane>
+					{/each}
+				</Splitpanes>
+			</Pane>
+		{/each}
+	</Splitpanes>
+{:else}
+	<Splitpanes theme="" class="p-4" horizontal={true}>
+		{#each $paneRows as rowPanes, i}
+			<Pane minSize={2}>
+				<Splitpanes theme="" horizontal={false}>
+					{#each rowPanes as pane, j}
+						<Pane minSize={2}>
+							<PaneContents text={pane.text} index={pane.index}/>
+						</Pane>
+					{/each}
+				</Splitpanes>
+			</Pane>
+		{/each}
+	</Splitpanes>
+{/if}
+
 
 
