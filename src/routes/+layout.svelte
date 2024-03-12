09e793be7f9b2083655f844f69a03cede003fb9d
tithe <tith3@proton.me>
2024-03-12 16:26:43 -0400

    Boiler plate to enable toasts.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,17 @@
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { AppBar, storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	import { initializeStores } from '@skeletonlabs/skeleton';
+	initializeStores();
+	import { Toast } from '@skeletonlabs/skeleton';
 
 	import { AppShell, LightSwitch} from '@skeletonlabs/skeleton';
 	import Icon from '$lib/components/Icon.svelte';
 	import HeaderControls from '$lib/components/HeaderControls.svelte';
+	
 </script>
 
+<Toast />
 <AppShell slotHeader='z-11'>
 	<svelte:fragment slot="header">
 		<AppBar padding='py-0 px-4 '>
