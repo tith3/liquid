dddcb727a1e5d4b6dedd9e550a170ceee523d83a
tithe <tith3@proton.me>
2024-03-12 02:28:50 -0400

    Aesthetic header controls.

---
+++ src/lib/components/HeaderControls.svelte
@@ -0,0 +1,10 @@
+<script> 
+    import { panes, addPane, resetPanes } from '$lib/stores/panes';
+    let text = '';
+</script>
+<div class="flex flex-row gap-4 h-8">
+    <input class="input" title="token address" type="text" placeholder="Enter token address" bind:value={text} />
+    <button type="button" class="btn variant-filled">Go</button>
+    <button type="button" class="btn variant-filled" on:click={() => {addPane(text)}}>Add Pane</button>
+    <button type="button" class="btn variant-filled" on:click={() => {resetPanes()}}>Reset Panes</button>
+</div>
