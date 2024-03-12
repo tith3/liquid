dbccfcd46cd725029aec3395cb7ed528f472bca1
tithe <tith3@proton.me>
2024-03-12 02:30:07 -0400

    Functional 3x3 tiling of pains. Not the most flexible but it will do for now.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,48 @@
 <script lang=ts>
 	import {Pane, Splitpanes} from "svelte-splitpanes"
 	import '$lib/css/splitpaneTheme.postcss'
+	import PaneContents from '$lib/components/PaneContents.svelte'
+	import {panes, addPane} from '$lib/stores/panes'
+	import {onMount} from 'svelte'
+	import { derived, get } from 'svelte/store'
+	let rowArr = [0]
+	//there must always be one pane
+	onMount(() => {
+		if(get(panes).length === 0) addPane('0')
+	})
+
+	function getPanesForRow(row: number) {
+		console.log('row', row);
+        return $panes.filter((pane) => {
+			console.log('pane', pane.index);
+			return Math.ceil((pane.index + 1) / 3) === row + 1;
+		}
+		)
+	}
+
+	$: paneRows = derived(panes, $panes => {
+        let rows = [];
+        let rowCount = Math.ceil($panes.length / 3);
+        for (let i = 0; i < rowCount; i++) {
+            rows.push($panes.slice(i * 3, (i + 1) * 3));
+        }
+        return rows;
+    });
+		
 </script>
 
 <Splitpanes theme="skeleton" class="p-4" horizontal={true}>
-	<Pane minSize={10}> One </Pane>
-	<Pane minSize={10}> Two </Pane>
+	{#each $paneRows as rowPanes, i}
+        <Pane minSize={2}>
+            <Splitpanes theme="skeleton" horizontal={false}>
+                {#each rowPanes as pane, j}
+                    <Pane minSize={2}>
+                        <PaneContents text={pane.text} index={pane.index}/>
+                    </Pane>
+                {/each}
+            </Splitpanes>
+        </Pane>
+    {/each}
 </Splitpanes>
 
 
