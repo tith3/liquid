fbabbfd51263a0412c47925d58db121c97eec55d
tithe <tith3@proton.me>
2024-03-18 20:34:41 -0400

    Changes to do with some resizing debugging and datapointname.

--- src/lib/stores/panes.ts
+++ src/lib/stores/panes.ts
@@ -1,11 +1,10 @@
 import {localStorageStore} from '@skeletonlabs/skeleton'
-import { type Writable, get } from 'svelte/store'
-import type { PaneData, PaneDimensions } from '../types'
-import { Pane } from 'svelte-splitpanes';
+import { type Writable, writable, get } from 'svelte/store'
+import { DSDataPointNames, type PaneData, type PaneDimensions } from '../types'
 
 export const panes: Writable<PaneData[]> = localStorageStore('panes', [] as PaneData[])
-
-
+export const resizing = writable(false); //stores whether a resize is going on
+export const resetKey: Writable<number>  = writable(0);
 //default pane dimensions
 const defaultPaneDimensions: PaneDimensions = {
     rows: 3,
@@ -14,17 +13,20 @@ const defaultPaneDimensions: PaneDimensions = {
     maxCols: 3,
     vertical: false,
 }
+
+
 //store the dimensions of the panes
 export const paneDimensions: Writable<PaneDimensions> = localStorageStore('paneDimensions', defaultPaneDimensions)
 
-export function addPane(text: string,) {
+export function addPane() {
     const maxPanes = get(paneDimensions).rows * get(paneDimensions).cols
     if (get(panes).length >= maxPanes) {
         return;
     }
     panes.update((panes) => {
         const index = panes.length
-        return [...panes, { index, text, headerExpanded: true}]
+        const dataPointName = DSDataPointNames.LIQUIDITY_QUOTE
+        return [...panes, { index, dataPointName, headerExpanded: true}]
     })
 }
 
@@ -43,12 +45,16 @@ export function removePane(index: number) {
 }
 
 export function resetPanes() {
-    const newPane = { index: 0, text: '0', headerExpanded: true}
+    const newPane = { index: 0, dataPointName: DSDataPointNames.LIQUIDITY_QUOTE, headerExpanded: true}
     panes.set([newPane])
 }
 
 export function toggleHeaderExpanded(index: number) {
     panes.update(panes => panes.map((pane, i) => i === index ? { ...pane, headerExpanded: !pane.headerExpanded } : pane));
+}
+
+export function setDataPointName(index: number, dataPointName: DSDataPointNames) { 
+    panes.update(panes => panes.map((pane, i) => i === index ? { ...pane, dataPointName } : pane));
 }
 
 export function shrinkPanes() {
@@ -58,4 +64,8 @@ export function shrinkPanes() {
     panes.update((panes) => {
         return panes.slice(0, maxPanes)
     })
+}
+
+export function resetPaneSize() {
+    resetKey.update((key) => key + 1)
 }
