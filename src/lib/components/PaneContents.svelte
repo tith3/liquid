c0aeae480faed2c8c294439e73715ff45a897dcc
tithe <tith3@proton.me>
2024-03-18 20:33:31 -0400

    Added header expantion, graph, data combobox. Lots of stuff I guess I didn't commit before.

--- src/lib/components/PaneContents.svelte
+++ src/lib/components/PaneContents.svelte
@@ -1,26 +1,93 @@
 <script lang='ts'>
-    import {AppShell, AppBar} from '@skeletonlabs/skeleton';  
-    import {removePane, toggleHeaderExpanded} from '$lib/stores/panes';  
+    import { Line } from 'svelte-chartjs'
+    import {AppShell, AppBar, type PopupSettings, ListBox, ListBoxItem, popup} from '@skeletonlabs/skeleton';  
+    import {removePane, toggleHeaderExpanded, setDataPointName} from '$lib/stores/panes';  
     import { X, CaretDoubleRight } from 'phosphor-svelte';
-    import type { PaneData } from '$lib/types';
-    
-    
-
+    import { type PaneData, DSDataPointNames } from '$lib/types';
+    import { dsDataSet } from '$lib/stores/dsdata';
+    import {getDataPointByName} from '$lib/utils/dsdata';
     export let paneData: PaneData;
-    $: ({text, index, headerExpanded} = paneData);
+    $: ({dataPointName, index, headerExpanded} = paneData);
     
+    // Graph
+    let data = {
+        labels: [] as string[],
+        datasets: [
+            {
+                label: dataPointName,
+                data: [] as number[],
+                lineTension: 0.1,
+            }
+        ]
+    };
 
+    $: {
+        //text = `Pane ${index}`;
+        data.labels = $dsDataSet.map((d) => new Date(d.time).toLocaleDateString());
+        console.log(dataPointName);
+        //get corresponding data points
+        data.datasets[0].data = $dsDataSet.map((d) => getDataPointByName(dataPointName, d));
+        //get corresponding label
+        data.datasets[0].label = dataPointName;
+    }
+
+    
+    /// Header Controls
+    //combobox
+    let PDCId = 'popupDataCombobox' + index;
+    let popupDataCombobox: PopupSettings = {
+        event: 'click',
+        target: PDCId,
+        placement: 'bottom',
+        closeQuery: '.listbox-item',
+    }
+    $: {
+        //check if index is undefined
+        if(index !== undefined){
+            PDCId = 'popupDataCombobox' + index;
+            popupDataCombobox = {
+                event: 'click',
+                target: PDCId,
+                placement: 'bottom',
+                closeQuery: '.listbox-item',
+            }
+        }
+    }
+
+    let dataCombobox = DSDataPointNames.LIQUIDITY_QUOTE;
+    $: {
+        console.log("Changing data point name " + dataCombobox);
+        //check if datacomboboc is not undegined
+        if(dataCombobox !== undefined){
+            setDataPointName(index, dataCombobox);
+        }
+    }
 </script>
 
-<AppShell class="variant-glass-surface rounded-lg">
+<!-- Popups -->
+<div class="card variant-filled text-xs p-1 z-50" data-popup={PDCId}>
+    <ListBox>
+        {#each Object.values(DSDataPointNames) as item}
+            <ListBoxItem bind:group={dataCombobox} name="medium" value={item} class="listbox-item">{item}</ListBoxItem>
+        {/each}
+    </ListBox>
+</div>
+<!-- /Popups -->
+
+<AppShell class="variant-glass-surface rounded-lg" slotHeader="z-11 {headerExpanded ? '' : 'absolute'} w-full" slotPageContent="size-full overflow-hidden z-11" >
     <svelte:fragment slot="header">
-        <AppBar padding='h-7 {headerExpanded ? 'w-full' : 'w-7'}' background='variant-ghost-surface rounded-t-lg {headerExpanded ? 'rounded-t-lg' : 'rounded-lg'}'>
+        <AppBar slotDefault="w-full" padding='h-7 {headerExpanded ? 'w-full' : 'w-7'}' background='variant-ghost-surface rounded-t-lg {headerExpanded ? 'rounded-t-lg' : 'rounded-lg'}'>
             <svelte:fragment slot="lead">
                 <button type= 'button' class='btn h-7 w-7 p-0 hover:variant-glass-surface {headerExpanded ? '' : 'rounded-lg'}' on:click={() => {toggleHeaderExpanded(index)}}>
                     <CaretDoubleRight class='fill-token {headerExpanded ? 'rotate-180' : 'rotate-0'}'/>
                 </button>
             </svelte:fragment>
-                <p class='text h-7'>{text}</p>
+                {#if headerExpanded}
+                    <button class="btn variant-filled justify-between h-5 text-xs" use:popup={popupDataCombobox}>
+                        <span>{dataPointName}</span>
+                        <span>↓</span>
+                    </button>
+                {/if}
             <svelte:fragment slot="trail">
                 <button type= 'button' class='btn h-7 w-7 p-0 top-0 right-0 absolute hover:variant-glass-surface ' on:click={() => {removePane(index)}}>
                     <X class='fill-token'/>
@@ -28,4 +95,8 @@
             </svelte:fragment>
         </AppBar>
     </svelte:fragment>
+    <Line {data} class="size-full" options ={{
+        responsive: true,
+        maintainAspectRatio: false,
+    }}/>
 </AppShell>
